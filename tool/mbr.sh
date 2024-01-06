71ff4732cb63c6e6bba407b86118bb32d14cd532
chisatowo <thy3972@outlook.com>
2024-01-06 17:14:08 +0800

    write a string to VGA_mem

--- tool/mbr.sh
+++ tool/mbr.sh
@@ -1,1 +1,2 @@
+nasm -o ~/Desktop/bochs_os-exp/boot/mbr.bin ~/Desktop/bochs_os-exp/boot/mbr.S
 dd if=/home/haoyu/Desktop/bochs_os-exp/boot/mbr.bin of=/home/haoyu/桌面/bochs/hd60M.img bs=512 count=1 conv=notrunc
