0ccd167f8f5dbb459bf6fbea3f808b3829117a73
chisatowo <thy3972@outlook.com>
2024-01-20 17:50:40 +0800

    finishes section 4: 1. modified loader.S to enable protect-mode and dd loader.bin into the 0x900 at disk hd60M.img. 2. modified mbr.S to call loader.S and load it into the memory.

--- tool/mbr.sh
+++ tool/mbr.sh
@@ -1,5 +1,5 @@
 nasm -I ~/Desktop/bochos/boot/include -o ~/Desktop/bochos/boot/mbr.bin ~/Desktop/bochos/boot/mbr.S
 nasm -I ~/Desktop/bochos/boot/include -o ~/Desktop/bochos/boot/loader.bin ~/Desktop/bochos/boot/loader.S
 dd if=/home/haoyu/Desktop/bochos/boot/mbr.bin of=/home/haoyu/桌面/bochs/hd60M.img bs=512 count=1 conv=notrunc
-dd if=/home/haoyu/Desktop/bochos/boot/loader.bin of=/home/haoyu/桌面/bochs/hd60M.img bs=512 count=1 seek=2 conv=notrunc
+dd if=/home/haoyu/Desktop/bochos/boot/loader.bin of=/home/haoyu/桌面/bochs/hd60M.img bs=512 count=3 seek=2 conv=notrunc
 # count表示要读取的块数,bs表示一个块的大小,seek表示要跳过的块数(由于此处loader是第2扇区,所以跳过两个块),conv指定如何转换文件,notrunc表示不打断文件.
